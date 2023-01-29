ea873f4edc69eba53a1e08cebbd2a12846f07cad
rivwgk <16526288+rivwgk@users.noreply.github.com>
2023-01-29 14:45:21 +0100

    uses grammar for parsing

--- 2022/04/main.pl
+++ 2022/04/main.pl
@@ -1,29 +1,12 @@
 :- use_module(library(clpfd)).
 :- use_module(library(apply)).
-:- use_module(library(lists)).
+:- use_module(library(dcg/basics)).
+:- use_module(library(pio)).
 
-stream_lines(Fd, Lines) :-
-   read_string(Fd, _, Str),
-   split_string(Str, "\n", "", Lines).
-
-ranges([], [], []).
-ranges([""], [], []).
-ranges([Str | T], [range(A, B) | T1], [range(C, D) | T2]) :-
-   split_string(Str, ",", "", [X, Y]),
-   split_string(X, "-", "", [SA, SB]),
-   split_string(Y, "-", "", [SC, SD]),
-   maplist(number_string, [A, B, C, D], [SA, SB, SC, SD]),
-   ranges(T, T1, T2).
-
-read_values(File, [R1, R2]) :-
-   setup_call_cleanup(
-      open(File, read, Fd, []),
-      (
-         stream_lines(Fd, Lines),
-         ranges(Lines, R1, R2)
-      ),
-      close(Fd)
-   ).
+ranges([], []) -->
+   [].
+ranges([range(A, B) | T1], [range(C, D) | T2]) -->
+   integer(A),"-",integer(B),",",integer(C),"-",integer(D),eol,ranges(T1, T2).
 
 compare1(range(A, B), range(C, D), X) :-
    X #<==> ((A #=< C #/\ D #=< B) #\/ (C #=< A #/\ B #=< D)).
@@ -32,11 +15,11 @@ compare2(range(A, B), range(C, D), X) :-
    X #<==> #\ (B #< C #\/ D #< A).
 
 task1(File, X) :-
-   read_values(File, [R1, R2]),
+   phrase_from_file(ranges(R1, R2), File),
    maplist(compare1, R1, R2, B),
    sumlist(B, X).
 
 task2(File, X) :-
-   read_values(File, [R1, R2]),
+   phrase_from_file(ranges(R1, R2), File),
    maplist(compare2, R1, R2, B),
    sumlist(B, X).
